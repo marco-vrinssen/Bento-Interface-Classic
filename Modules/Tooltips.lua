78c8e4783c492d58aff061565a6982c890036dc6
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-03-18 00:25:33 +0100

    Remove item level display functionality from tooltips for cleaner interface

--- Modules/Tooltips.lua
+++ Modules/Tooltips.lua
@@ -13,24 +13,3 @@ end
 GameTooltipStatusBar:SetScript("OnShow", function()
     GameTooltipStatusBar:Hide()
 end)
-
-
--- ADD ITEM LEVEL TO TOOLTIP
-
-local function addItemLevelToTooltip(tooltip)
-    local _, itemLink = tooltip:GetItem()
-    if itemLink then
-        local itemType = select(6, GetItemInfo(itemLink))
-        if itemType == "Armor" or itemType == "Weapon" or itemType == "Consumable" or itemType == "Reagent" or itemType == "Trade Goods" then
-            local itemLevel = select(4, GetItemInfo(itemLink))
-            if itemLevel then
-                tooltip:AddLine("Item Level: " .. itemLevel, unpack(GREY))
-                tooltip:Show()
-            end
-        end
-    end
-end
-
-GameTooltip:HookScript("OnTooltipSetItem", addItemLevelToTooltip)
-
-hooksecurefunc("GameTooltip_SetDefaultAnchor", updateTooltipPos)
