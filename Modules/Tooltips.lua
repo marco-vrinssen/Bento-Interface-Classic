fa022a0b6cc389c279c4720030d585bd0787a060
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-03-18 15:48:43 +0100

    Update tooltip position for improved alignment and visibility

--- Modules/Tooltips.lua
+++ Modules/Tooltips.lua
@@ -3,13 +3,14 @@
 local function updateTooltipPos(self)
     if self:GetAnchorType() ~= "ANCHOR_CURSOR" then
         GameTooltip:ClearAllPoints()
-        GameTooltip:SetPoint("TOPLEFT", TargetFramePortrait, "BOTTOMRIGHT", 4, -4)
+        GameTooltip:SetPoint("TOPLEFT", TargetPortraitBackdrop, "BOTTOMRIGHT", 0, 0)
     end
 end
-
 
 -- HIDE GAME TOOLTIP STATUS BAR
 
 GameTooltipStatusBar:SetScript("OnShow", function()
     GameTooltipStatusBar:Hide()
 end)
+ 
+hooksecurefunc("GameTooltip_SetDefaultAnchor", updateTooltipPos)
