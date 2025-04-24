806d737c93045c46e03836e1d9223ca30c7302a7
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-04-24 09:11:40 +0200

    Refactor PlayerPet.lua: Rename PetFrameBackdrop to petContainer for improved clarity and consistency in pet frame management

--- Modules/PlayerPet.lua
+++ Modules/PlayerPet.lua
@@ -1,32 +1,39 @@
--- CREATE PET FRAME BACKDROP
+-- CREATE PET FRAME CONTAINER WITH BORDER
 
-local PetFrameBackdrop = CreateFrame("Button", nil, PetFrame, "SecureUnitButtonTemplate, BackdropTemplate")
-PetFrameBackdrop:SetPoint("BOTTOMRIGHT", PlayerPortraitBackdrop, "BOTTOMLEFT", 0, 0)
-PetFrameBackdrop:SetSize(64, 24)
-PetFrameBackdrop:SetBackdrop({edgeFile = BORD, edgeSize = 12})
-PetFrameBackdrop:SetBackdropBorderColor(unpack(GREY))
-PetFrameBackdrop:SetFrameLevel(PetFrame:GetFrameLevel() + 2)
-PetFrameBackdrop:SetAttribute("unit", "pet")
-PetFrameBackdrop:RegisterForClicks("AnyUp")
-PetFrameBackdrop:SetAttribute("type1", "target")
-PetFrameBackdrop:SetAttribute("type2", "togglemenu")
+local petContainer = CreateFrame("Button", nil, PetFrame, "SecureUnitButtonTemplate, BackdropTemplate")
+petContainer:SetPoint("BOTTOMRIGHT", PlayerPortraitBackdrop, "BOTTOMLEFT", 0, 0)
+petContainer:SetSize(64, 24)
+petContainer:SetBackdrop({edgeFile = BORD, edgeSize = 12})
+petContainer:SetBackdropBorderColor(unpack(GREY))
+petContainer:SetFrameLevel(PetFrame:GetFrameLevel() + 2)
+petContainer:SetAttribute("unit", "pet")
+petContainer:RegisterForClicks("AnyUp")
+petContainer:SetAttribute("type1", "target")
+petContainer:SetAttribute("type2", "togglemenu")
 
+-- CREATE BACKGROUND FRAME BELOW BORDER
+
+local petContainerBackground = CreateFrame("Frame", nil, petContainer, "BackdropTemplate")
+petContainerBackground:SetPoint("TOPLEFT", petContainer, "TOPLEFT", 2, -2)
+petContainerBackground:SetPoint("BOTTOMRIGHT", petContainer, "BOTTOMRIGHT", -2, 2)
+petContainerBackground:SetBackdrop({ bgFile = BG })
+petContainerBackground:SetBackdropColor(unpack(BLACK))
+petContainerBackground:SetFrameLevel(petContainer:GetFrameLevel() - 1)
 
 -- UPDATE PET FRAME
 
 local function updatePetFrame()
 	PetFrame:ClearAllPoints()
-	PetFrame:SetPoint("CENTER", PetFrameBackdrop, "CENTER", 0, 0)
-	PetFrame:SetSize(PetFrameBackdrop:GetWidth(), PetFrameBackdrop:GetHeight())
+	PetFrame:SetPoint("CENTER", petContainer, "CENTER", 0, 0)
+	PetFrame:SetSize(petContainer:GetWidth(), petContainer:GetHeight())
     PetFrame:UnregisterEvent("UNIT_COMBAT")
 	PetAttackModeTexture:SetTexture(nil)
     PetFrameTexture:Hide()
-   
     PetPortrait:Hide()
 
     PetName:ClearAllPoints()
-    PetName:SetPoint("BOTTOMRIGHT", PetFrameBackdrop, "TOPRIGHT", -2, 2)
-    PetName:SetWidth(PetFrameBackdrop:GetWidth() - 4)
+    PetName:SetPoint("BOTTOMRIGHT", petContainer, "TOPRIGHT", -2, 2)
+    PetName:SetWidth(petContainer:GetWidth() - 4)
     PetName:SetFont(FONT, 10, "OUTLINE")
     PetName:SetTextColor(unpack(WHITE))
 
@@ -50,19 +57,18 @@ petFrameEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 petFrameEvents:RegisterEvent("UNIT_PET")
 petFrameEvents:SetScript("OnEvent", updatePetFrame)
 
-
 -- UPDATE PET RESOURCES
 
 local function UpdatePetResources()
     PetFrameHealthBar:ClearAllPoints()
-    PetFrameHealthBar:SetPoint("TOP", PetFrameBackdrop, "TOP", 0, -2)
+    PetFrameHealthBar:SetPoint("TOP", petContainer, "TOP", 0, -2)
     PetFrameHealthBar:SetPoint("BOTTOM", PetFrameManaBar, "TOP", 0, 0)
-    PetFrameHealthBar:SetWidth(PetFrameBackdrop:GetWidth()-6)
+    PetFrameHealthBar:SetWidth(petContainer:GetWidth()-6)
     PetFrameHealthBar:SetStatusBarTexture(BAR)
     
     PetFrameManaBar:ClearAllPoints()
-    PetFrameManaBar:SetPoint("BOTTOM", PetFrameBackdrop, "BOTTOM", 0, 2)
-    PetFrameManaBar:SetWidth(PetFrameBackdrop:GetWidth()-6)
+    PetFrameManaBar:SetPoint("BOTTOM", petContainer, "BOTTOM", 0, 2)
+    PetFrameManaBar:SetWidth(petContainer:GetWidth()-6)
     PetFrameManaBar:SetHeight(8)
     PetFrameManaBar:SetStatusBarTexture(BAR)
     
@@ -75,9 +81,8 @@ local function UpdatePetResources()
     PetFrameManaBarTextRight:SetAlpha(0)
 
     PetFrameHappiness:ClearAllPoints()
-    PetFrameHappiness:SetPoint("RIGHT", PetFrameBackdrop, "LEFT", 0, 0)
+    PetFrameHappiness:SetPoint("RIGHT", petContainer, "LEFT", 0, 0)
     PetFrameHappiness:SetSize(20, 20)
-
 end
 
 local petResourceEvents = CreateFrame("Frame")
