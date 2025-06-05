9c046e0dfa038f7004cbe128e5fa6d5c1204e3cc
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-06-05 18:05:32 +0200

    Add Player module: Implement combat indicator with backdrop and icon, and update visibility based on combat status

--- Modules/Player.lua
+++ Modules/Player.lua
@@ -221,3 +221,37 @@ playerGroupEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 playerGroupEvents:RegisterEvent("GROUP_ROSTER_UPDATE")
 playerGroupEvents:RegisterEvent("PARTY_LEADER_CHANGED")
 playerGroupEvents:SetScript("OnEvent", updatePlayerGroup)
+
+-- CREATE PLAYER COMBAT INDICATOR
+
+local playerCombatBackdrop = CreateFrame("Frame", nil, PlayerFrame, "BackdropTemplate")
+playerCombatBackdrop:SetPoint("BOTTOMRIGHT", PlayerPortraitBackdrop, "TOPLEFT", 2, 2)
+playerCombatBackdrop:SetSize(28, 28)
+playerCombatBackdrop:SetBackdrop({
+    bgFile = BG,
+    edgeFile = BORD, edgeSize = 12,
+    insets = {left = 3, right = 3, top = 3, bottom = 3}
+})
+playerCombatBackdrop:SetBackdropColor(unpack(BLACK_RGB))
+playerCombatBackdrop:SetBackdropBorderColor(unpack(GREY_RGB))
+playerCombatBackdrop:Hide()
+
+local playerCombatIcon = playerCombatBackdrop:CreateTexture(nil, "OVERLAY")
+playerCombatIcon:SetPoint("CENTER", playerCombatBackdrop, "CENTER", 0, 0)
+playerCombatIcon:SetSize(16 * 1.2, 16 * 1.2)
+playerCombatIcon:SetTexture("Interface\\CharacterFrame\\UI-StateIcon")
+playerCombatIcon:SetTexCoord(0.5, 1.0, 0.0, 0.5)
+
+local function updatePlayerCombat()
+    if UnitAffectingCombat("player") then
+        playerCombatBackdrop:Show()
+    else
+        playerCombatBackdrop:Hide()
+    end
+end
+
+local playerCombatEvents = CreateFrame("Frame")
+playerCombatEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
+playerCombatEvents:RegisterEvent("PLAYER_REGEN_ENABLED")
+playerCombatEvents:RegisterEvent("PLAYER_REGEN_DISABLED")
+playerCombatEvents:SetScript("OnEvent", updatePlayerCombat)
